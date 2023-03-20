eb9c743bd155058fc77b3a2e0b27f6197f95fbec
Jhon Waree Marcelo <jhon.waree.marcelo@gmail.com>
2023-03-20 00:05:40 -0400

    Completed Functions:Continued

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+function saturdayFun(activity = 'roller-skate', msg = 'This Saturday, I want to '){
+    return `${msg}${activity}!`
+}
+
+function mondayWork(activity = 'go to the office',msg = 'This Monday, I will '){
+    return `${msg}${activity}.`;
+}
+
+function wrapAdjective(wrapper){
+    return function(adjective) {
+        return `You are ${wrapper}${adjective}${wrapper}!`;
+    }
+}
